394fefb6f5a7d76e970dc9e9003f159c175b98f7
Денис <70154413+ktiw@users.noreply.github.com>
2025-07-16 21:28:50 +0700

    Twentythreetask

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
        fileHandler.writeActionLog("actions.log", "Игрок Alex зашел на сервер.");
        fileHandler.writeActionLog("actions.log", "Игрок Max_Pvp убил зомби.");
        fileHandler.writeActionLog("actions.log", "Игрок Newbie нашел алмазы!.");
-
     }
 }
+
